50796d27db9abced39163e302fb1a8f16c38ec90
Eduardo Vera <this.eduardovera@gmail.com>
2025-06-13 21:06:38 -0400

    making layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
+    return (
+        <body>
+            <main className="container">
+                <header className="nav">
+                    <h1>Sazon</h1>
+                    <nav>seachbar</nav>
+                </header>
+                <aside className="aside">aside</aside>
+                <section className="content">content</section>
+                <footer>footer</footer>
+            </main>
+        </body>
+    );
 }
 
 export default App;

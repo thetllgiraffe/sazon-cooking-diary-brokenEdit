691f64752e40c7d569cd2f833059fab27c0d00d9
Eduardo Vera <this.eduardovera@gmail.com>
2025-06-16 01:02:36 -0400

    Creating content section

---
+++ src/views/content/Content.tsx
@@ -0,0 +1,78 @@
+import type { Meal } from "../../models/Imeals";
+
+import "./content.css";
+
+export function Content({ currentMeal }: { currentMeal: Meal }) {
+    console.log(currentMeal);
+
+    const allIngredients = Object.entries(currentMeal).filter(
+        ([key, val]) => key.includes("strIngredient") && val,
+    );
+
+    const allMeasures = Object.entries(currentMeal).filter(
+        ([key, val]) => key.includes("strMeasure") && val,
+    );
+
+    const allIngredientsAndMeasures = Array.from({ length: allIngredients.length }, (_, i) => [
+        allIngredients[i][1],
+        allMeasures[i][1],
+    ]);
+
+    const preparation = currentMeal?.strInstructions.split(".").filter((step) => step);
+
+    console.log(preparation);
+
+    return (
+        <section className="content">
+            <div
+                className="content--image"
+                style={{
+                    backgroundImage: `
+                        linear-gradient(to right, rgba(141, 135, 65, 0.802), rgba(141, 135, 65, 0.704)), 
+                        url(${currentMeal.strMealThumb})
+                    `,
+                }}
+            >
+                <h3>{currentMeal.strMeal}</h3>
+            </div>
+
+            <div className="ingredients__section">
+                <h3>Recipe Ingredients</h3>
+
+                <ul className="ingredients">
+                    {allIngredientsAndMeasures?.map(([ingredient, measure]) => {
+                        return (
+                            <li>
+                                <span>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="#000000"
+                                        viewBox="0 0 256 256"
+                                    >
+                                        <path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"></path>
+                                    </svg>
+                                    {measure}
+                                </span>
+                                <span>{ingredient}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+
+            <div className="preparation__section">
+                <h3>How to cook it</h3>
+                <ol>
+                    {preparation?.map((step, i) => (
+                        <li>
+                            <span>{i + 1}. </span>
+                            <span>{step.trim()}.</span>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        </section>
+    );
+}
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,2 +1,3 @@
 export { Header } from "./header/Header";
 export { SearchResults } from "./aside/Aside";
+export { Content } from "./content/Content";
